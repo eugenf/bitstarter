26437a511c324e507a4762c8b1235d3b4ce66321
Eugen F <eugen@figursky.com>
2013-07-08 19:47:50 +0000

    added index.html file to app

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var file = fs.readFileSync('index.html');
+var buffer = new Buffer(file);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
